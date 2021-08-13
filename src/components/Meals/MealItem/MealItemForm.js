2c41de6d633fc2f4bd39a1975db93cd16caf90c0
Alexander Botwinick <alexbotwinick@gmail.com>
2021-08-13 09:57:50 +0200

    add amt validation state and logic, along with ref

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 import Input from '../../UI/Input/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNum = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNum < 1 ||
+      enteredAmountNum > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNum);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label='Amount'
         input={{
           type: 'number',
@@ -18,6 +40,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>Add to Cart</button>
+      {!amountIsValid && <p>Please enter a valid amount between 1 and 5.</p>}
     </form>
   );
 };
