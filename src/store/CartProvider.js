e095c00daf3201f5aea252dfe17de0f644c7620d
Alexander Botwinick <alexbotwinick@gmail.com>
2021-08-23 15:36:47 +0200

    add clearCart functionality into reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -60,6 +60,10 @@ const cartReducer = (state, action) => {
     };
   }
 
+  if (action.type === 'CLEAR') {
+    return defaultCartState;
+  }
+
   return defaultCartState;
 };
 
@@ -75,11 +79,16 @@ const CartProvider = (props) => {
     dispatchCart({ type: 'REMOVE_ITEM', id: id });
   };
 
+  const clearCartHandler = () => {
+    dispatchCart({ type: 'CLEAR' });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
 
   return (
