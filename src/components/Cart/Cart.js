00a3f44b095f22acb696eff7a78017243adffe4d
Alexander Botwinick <alexbotwinick@gmail.com>
2021-08-23 15:36:15 +0200

    add state and functionality wrt to POSTing order

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,10 +5,15 @@ import Modal from '../UI/Modal/Modal';
 import CartDeliveryForm from './CartDeliveryForm';
 
 import CartContext from '../../store/cart-context';
+import { REQUEST_URL } from '../../js/config';
 
 import classes from './Cart.module.css';
 
 const Cart = (props) => {
+  const [showDeliveryForm, setShowDeliveryForm] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
+
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -36,10 +41,25 @@ const Cart = (props) => {
     </ul>
   );
 
-  // *****************************************
-  // ORDER FORM - NAME AND ADDRESS ***********
-  // *****************************************
-  const [showDeliveryForm, setShowDeliveryForm] = useState(false);
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    try {
+      const response = await fetch(`${REQUEST_URL}/orders.json`, {
+        method: 'POST',
+        body: JSON.stringify({
+          user: userData,
+          orderedItems: cartCtx.items,
+        }),
+      });
+      if (response.ok) {
+        setIsSubmitting(false);
+        setDidSubmit(true);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    cartCtx.clearCart();
+  };
 
   const showDeliveryFormHandler = (e) => {
     if (hasItems) {
@@ -60,15 +80,39 @@ const Cart = (props) => {
     </div>
   );
 
-  return (
-    <Modal onClose={props.onClose}>
+  const cartModalContent = (
+    <React.Fragment>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
       {!showDeliveryForm && orderActions}
-      {showDeliveryForm && <CartDeliveryForm onClose={props.onClose} />}
+      {showDeliveryForm && (
+        <CartDeliveryForm
+          onClose={props.onClose}
+          onConfirm={submitOrderHandler}
+        />
+      )}
+    </React.Fragment>
+  );
+
+  const isSubmittingModalContent = <p>Sending order data...</p>;
+
+  const didSubmitModalContent = (
+    <React.Fragment>
+      <p>Thank you, your order was received!</p>
+      <button className={classes.button} onClick={props.onClose}>
+        Close
+      </button>
+    </React.Fragment>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {!isSubmitting && !didSubmit && cartModalContent}
+      {isSubmitting && isSubmittingModalContent}
+      {!isSubmitting && didSubmit && didSubmitModalContent}
     </Modal>
   );
 };
